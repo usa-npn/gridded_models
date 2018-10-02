719354bf18ad66c850bd0e9aba9f01ca28101bd8
Jeff Switzer <jeff.switzer@gmail.com>
2018-10-02 12:18:31 -0700

    extra paren

--- util/gdd.py
+++ util/gdd.py
@@ -207,11 +207,11 @@ def dynamic_agdd(start_date, num_days, base, climate_data_provider, region):
 
             if climate_data_provider == 'prism':
                 tavg_tif_path = "/geo-vault/climate_data/prism/prism_data/tavg/tavg_{day}.tif"
-                .format(day=day.strftime("%Y%m%d"))
+                .format(day=day.strftime("%Y%m%d")
 
             if climate_data_provider == 'ncep':
                 tavg_tif_path = "/geo-data/climate_data/daily_data/tavg/tavg_{day}.tif"
-                .format(day=day.strftime("%Y%m%d"))
+                .format(day=day.strftime("%Y%m%d")
             
             tavg = get_climate_data_from_file(tavg_tif_path)
 
