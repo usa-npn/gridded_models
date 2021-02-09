7eb74928d9e939c5d293d3d9d7b6ac0cb82faaf3
JEFF SWITZER <npn@on-campus-10-138-79-192.vpn.arizona.edu>
2021-02-09 09:46:06 -0700

    code cleanup

--- compute_winter_wheat.py
+++ compute_winter_wheat.py
@@ -22,374 +22,7 @@ from prism.importer import get_prism_data_outdb
 import numpy as np
 from osgeo import gdal
 from util.raster import *
-
-
-solar_declination = [307,
-                     308,
-                     309,
-                     310,
-                     311,
-                     312,
-                     313,
-                     314,
-                     315,
-                     316,
-                     317,
-                     318,
-                     319,
-                     320,
-                     321,
-                     322,
-                     323,
-                     324,
-                     325,
-                     326,
-                     327,
-                     328,
-                     329,
-                     330,
-                     331,
-                     332,
-                     333,
-                     334,
-                     335,
-                     336,
-                     337,
-                     338,
-                     339,
-                     340,
-                     341,
-                     342,
-                     343,
-                     344,
-                     345,
-                     346,
-                     347,
-                     348,
-                     349,
-                     350,
-                     351,
-                     352,
-                     353,
-                     354,
-                     355,
-                     356,
-                     357,
-                     358,
-                     359,
-                     360,
-                     361,
-                     362,
-                     363,
-                     364,
-                     365,
-                     366,
-                     1,
-                     2,
-                     3,
-                     4,
-                     5,
-                     6,
-                     7,
-                     8,
-                     9,
-                     10,
-                     11,
-                     12,
-                     13,
-                     14,
-                     15,
-                     16,
-                     17,
-                     18,
-                     19,
-                     20,
-                     21,
-                     22,
-                     23,
-                     24,
-                     25,
-                     26,
-                     27,
-                     28,
-                     29,
-                     30,
-                     31,
-                     32,
-                     33,
-                     34,
-                     35,
-                     36,
-                     37,
-                     38,
-                     39,
-                     40,
-                     41,
-                     42,
-                     43,
-                     44,
-                     45,
-                     46,
-                     47,
-                     48,
-                     49,
-                     50,
-                     51,
-                     52,
-                     53,
-                     54,
-                     55,
-                     56,
-                     57,
-                     58,
-                     59,
-                     60,
-                     61,
-                     62,
-                     63,
-                     64,
-                     65,
-                     66,
-                     67,
-                     68,
-                     69,
-                     70,
-                     71,
-                     72,
-                     73,
-                     74,
-                     75,
-                     76,
-                     77,
-                     78,
-                     79,
-                     80,
-                     81,
-                     82,
-                     83,
-                     84,
-                     85,
-                     86,
-                     87,
-                     88,
-                     89,
-                     90,
-                     91,
-                     92,
-                     93,
-                     94,
-                     95,
-                     96,
-                     97,
-                     98,
-                     99,
-                     100,
-                     101,
-                     102,
-                     103,
-                     104,
-                     105,
-                     106,
-                     107,
-                     108,
-                     109,
-                     110,
-                     111,
-                     112,
-                     113,
-                     114,
-                     115,
-                     116,
-                     117,
-                     118,
-                     119,
-                     120,
-                     121,
-                     122,
-                     123,
-                     124,
-                     125,
-                     126,
-                     127,
-                     128,
-                     129,
-                     130,
-                     131,
-                     132,
-                     133,
-                     134,
-                     135,
-                     136,
-                     137,
-                     138,
-                     139,
-                     140,
-                     141,
-                     142,
-                     143,
-                     144,
-                     145,
-                     146,
-                     147,
-                     148,
-                     149,
-                     150,
-                     151,
-                     152,
-                     153,
-                     154,
-                     155,
-                     156,
-                     157,
-                     158,
-                     159,
-                     160,
-                     161,
-                     162,
-                     163,
-                     164,
-                     165,
-                     166,
-                     167,
-                     168,
-                     169,
-                     170,
-                     171,
-                     172,
-                     173,
-                     174,
-                     175,
-                     176,
-                     177,
-                     178,
-                     179,
-                     180,
-                     181,
-                     182,
-                     183,
-                     184,
-                     185,
-                     186,
-                     187,
-                     188,
-                     189,
-                     190,
-                     191,
-                     192,
-                     193,
-                     194,
-                     195,
-                     196,
-                     197,
-                     198,
-                     199,
-                     200,
-                     201,
-                     202,
-                     203,
-                     204,
-                     205,
-                     206,
-                     207,
-                     208,
-                     209,
-                     210,
-                     211,
-                     212,
-                     213,
-                     214,
-                     215,
-                     216,
-                     217,
-                     218,
-                     219,
-                     220,
-                     221,
-                     222,
-                     223,
-                     224,
-                     225,
-                     226,
-                     227,
-                     228,
-                     229,
-                     230,
-                     231,
-                     232,
-                     233,
-                     234,
-                     235,
-                     236,
-                     237,
-                     238,
-                     239,
-                     240,
-                     241,
-                     242,
-                     243,
-                     244,
-                     245,
-                     246,
-                     247,
-                     248,
-                     249,
-                     250,
-                     251,
-                     252,
-                     253,
-                     254,
-                     255,
-                     256,
-                     257,
-                     258,
-                     259,
-                     260,
-                     261,
-                     262,
-                     263,
-                     264,
-                     265,
-                     266,
-                     267,
-                     268,
-                     269,
-                     270,
-                     271,
-                     272,
-                     273,
-                     274,
-                     275,
-                     276,
-                     277,
-                     278,
-                     279,
-                     280,
-                     281,
-                     282,
-                     283,
-                     284,
-                     285,
-                     286,
-                     287,
-                     288,
-                     289,
-                     290,
-                     291,
-                     292,
-                     293,
-                     294,
-                     295,
-                     296,
-                     297,
-                     298,
-                     299,
-                     300,
-                     301,
-                     302,
-                     303,
-                     304,
-                     305,
-                     306]
+from spring_index.solar_declination import solar_declination
 
 
 with open(os.path.abspath(os.path.join(os.path.dirname(__file__), 'config.yml')), 'r') as ymlfile:
@@ -406,9 +39,10 @@ def vernalization_days(tavg):
     vd[(vd >= 44.6) & (vd <= 64.4)] = -(0.06 * vd[(vd >= 44.6) & (vd <= 64.4)]) + 3.38
     return vd
 
+# returns array[day, lat] = [daylength at each longitude]
 def photoperiod(upper_left_y):
-    num_lats = 1228#621
-    num_longs = 2606#1405
+    num_lats = 1228
+    #num_longs = 2606
     day_max = 365
 
     ydim = 0.04166666666667
@@ -420,7 +54,7 @@ def photoperiod(upper_left_y):
         site_latitudes *= -ydim
         site_latitudes += upper_left_y
 
-        # calculate day lengths and rounded day lengths
+        # calculate day lengths
         site_day_lengths = np.empty((day_max, num_lats))
         for day in range(0, day_max):
             temp_lats = np.copy(site_latitudes)
@@ -449,33 +83,31 @@ def compute_winter_wheat(start_date, stop_date):
         try:
             tmin = get_climate_data_from_file(tmin_tif_path, temp_unit)
             tmax = get_climate_data_from_file(tmax_tif_path, temp_unit)
+            tmax[tmax > 78.8] = 78.8
+            tavg = (tmin + tmax) / 2
             num_lats = tmin.shape[0]
             num_lngs = tmin.shape[1]
         except:
-            logging.error('could not get temp data, aborting agdd computation: ', exc_info=True)
+            logging.error('could not get temp data, aborting winter wheat computation: ', exc_info=True)
             return
         if doy == 0:
             ds = gdal.Open(tmin_tif_path)
             projection = ds.GetProjection()
             transform = ds.GetGeoTransform()
             (upper_left_x, x_size, x_rotation, upper_left_y, y_rotation, y_size) = transform
+            ds = None
+            # compute photoperiod penalty
             daylengths = photoperiod(upper_left_y)
             photoperiod_penalty = 0.075 * daylengths - 0.24
-            ds = None
-
-        tmax[tmax > 78.8] = 78.8
-        tavg = (tmin + tmax) / 2
-
-        # compute vernalization days and photoperiod
-        if doy == 0:
+            # intialize vernalization days accum and agdd to zeros
             vd_accum = np.zeros_like(tmin)
-            gdd = np.zeros_like(tmin)
             agdd = np.zeros_like(tmin)
+        # compute vernalization days, add it to the accum, and calculate the penalty from the accum
         vd = vernalization_days(tavg)
         vd_accum = vd_accum + vd
         vd_penalty = 0.015 * vd_accum + 0.21
 
-        # optimized version
+        # begin optimized version of algorithm
         bases = np.where(agdd <= 752, 35.6, 32)
         gdd = tavg - bases
         gdd[gdd < 0] = 0
@@ -486,8 +118,9 @@ def compute_winter_wheat(start_date, stop_date):
         penalties = np.where((vd_accum < 50) & (photoperiod_penalty_broadcasted > vd_penalty), vd_penalty, photoperiod_penalty_broadcasted)
         gdd *= penalties
         agdd += gdd
+        # end optimized version of algorithm
 
-        # nonoptimized version
+        # begin nonoptimized version of algorithm
         # for lat in range(0, num_lats):
         #     for lng in range(0, num_lngs):
         #         base = 35.6
@@ -502,6 +135,7 @@ def compute_winter_wheat(start_date, stop_date):
         #             else:
         #                 gdd[lat, lng] = (tavg[lat, lng] - base) * photoperiod_penalty[doy, lat]
         #         agdd[lat, lng] += gdd[lat, lng]
+        # end nonoptimized version of algorithm
         
         # write the raster to disk
         winter_wheat_path = "/geo-data/gridded_models/winter_wheat/winter_wheat_{day}.tif".format(day=day.strftime("%Y%m%d"))
@@ -522,6 +156,8 @@ def main():
 
     start_date = "2020-01-01"
     stop_date = "2020-12-31"
+    # start_date = "2021-01-01"
+    # stop_date = "2021-01-31"
     #stop_date = date.today().strftime("%Y-%m-%d")
     compute_winter_wheat(start_date, stop_date)
 
