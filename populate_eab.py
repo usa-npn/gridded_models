b5f0e23c3d5d64c8e586da2b1c6e3336e2ace841
jeffswitzer <jeff.switzer@gmail.com>
2023-02-13 09:24:26 -0700

    url fix

--- populate_eab.py
+++ populate_eab.py
@@ -4,8 +4,8 @@ import requests
 today = date.today()
 current_year = today.year
 
-eab_adult_url = f'https://uspest.org/CAPS/EAB_cohorts/Earliest_PEMp0Excl1_{current_year}1231.tif'
-eab_egg_url = f'https://uspest.org/CAPS/EAB_cohorts/Earliest_PEMe1Excl1_{current_year}1231.tif'
+eab_adult_url = f'https://uspest.org/CAPS/EAB_cohorts/Misc_output/Earliest_PEMp0Excl1_{current_year}1231.tif'
+eab_egg_url = f'https://uspest.org/CAPS/EAB_cohorts/Misc_output/Earliest_PEMe1Excl1_{current_year}1231.tif'
 
 
 with open(f'/geo-data/gridded_models/eab/eab_adult/eab_adult_{date.today().strftime("%Y-%m-%d")}.tif', 'wb') as out_file:
