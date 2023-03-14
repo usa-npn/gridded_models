2035a44ad66c6e80b60c32b586b5e048902acaa3
jeffswitzer <jeff.switzer@gmail.com>
2023-03-14 09:41:36 -0700

    eab timeseries update

--- populate_eab.py
+++ populate_eab.py
@@ -1,17 +1,29 @@
 from datetime import date
 import requests
+from util.database import update_time_series
+from util.database import table_exists
 
 today = date.today()
 current_year = today.year
+today_as_string = today.strftime("%Y-%m-%d")
 
 eab_adult_url = f'https://uspest.org/CAPS/EAB_cohorts/Misc_output/Earliest_PEMp0Excl1_{current_year}1231.tif'
 eab_egg_url = f'https://uspest.org/CAPS/EAB_cohorts/Misc_output/Earliest_PEMe1Excl1_{current_year}1231.tif'
 
-
-with open(f'/geo-data/gridded_models/eab/eab_adult/eab_adult_{date.today().strftime("%Y%m%d")}.tif', 'wb') as out_file:
+filename = f'/geo-data/gridded_models/eab/eab_adult/eab_adult_{date.today().strftime("%Y%m%d")}.tif'
+with open(filename, 'wb') as out_file:
    content = requests.get(eab_adult_url, stream=True).content
    out_file.write(content)
+   time_series_table = 'eab_adult'
+   tif_name = f'eab_adult_{date.today().strftime("%Y%m%d")}.tif'
+   if table_exists(time_series_table):
+      update_time_series(time_series_table, tif_name, today_as_string)
 
-with open(f'/geo-data/gridded_models/eab/eab_egg/eab_egg_{date.today().strftime("%Y%m%d")}.tif', 'wb') as out_file:
+filename = f'/geo-data/gridded_models/eab/eab_egg_hatch/eab_egg_hatch_{date.today().strftime("%Y%m%d")}.tif'
+with open(filename, 'wb') as out_file:
    content = requests.get(eab_egg_url, stream=True).content
    out_file.write(content)
+   time_series_table = 'eab_egg_hatch'
+   tif_name = f'eab_egg_hatch_{date.today().strftime("%Y%m%d")}.tif'
+   if table_exists(time_series_table):
+      update_time_series(time_series_table, tif_name, today_as_string)
