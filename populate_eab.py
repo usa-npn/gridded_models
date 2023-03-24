3f9a2f082004f04f2fdf873d2cd56ff70aa7605e
jeffswitzer <jeff.switzer@gmail.com>
2023-03-24 13:13:33 -0700

    set eab srs

--- populate_eab.py
+++ populate_eab.py
@@ -2,6 +2,8 @@ from datetime import date
 import requests
 from util.database import update_time_series
 from util.database import table_exists
+import subprocess
+import os.path
 
 today = date.today()
 current_year = today.year
@@ -9,6 +11,17 @@ today_as_string = today.strftime("%Y-%m-%d")
 
 eab_adult_url = f'https://uspest.org/CAPS/EAB_cohorts/Misc_output/Earliest_PEMp0Excl1_{current_year}1231.tif'
 eab_egg_url = f'https://uspest.org/CAPS/EAB_cohorts/Misc_output/Earliest_PEMe1Excl1_{current_year}1231.tif'
+
+def set_srs(tif_file):
+    temp_file = str.replace(tif_file, ".tif", "_warpme.tif")
+    os.rename(tif_file, temp_file)
+
+    warp_command = "gdalwarp -t_srs EPSG:4269 {source_file} {dest_file}"\
+        .format(source_file=temp_file, dest_file=tif_file)
+
+    ps = subprocess.Popen(warp_command, stdout=subprocess.PIPE, shell=True)
+    ps.wait()
+    os.remove(temp_file)
 
 filename = f'/geo-data/gridded_models/eab/eab_adult/eab_adult_{date.today().strftime("%Y%m%d")}.tif'
 with open(filename, 'wb') as out_file:
@@ -18,6 +31,7 @@ with open(filename, 'wb') as out_file:
    tif_name = f'eab_adult_{date.today().strftime("%Y%m%d")}.tif'
    if table_exists(time_series_table):
       update_time_series(time_series_table, tif_name, today_as_string)
+set_srs(filename)
 
 filename = f'/geo-data/gridded_models/eab/eab_egg_hatch/eab_egg_hatch_{date.today().strftime("%Y%m%d")}.tif'
 with open(filename, 'wb') as out_file:
@@ -27,3 +41,4 @@ with open(filename, 'wb') as out_file:
    tif_name = f'eab_egg_hatch_{date.today().strftime("%Y%m%d")}.tif'
    if table_exists(time_series_table):
       update_time_series(time_series_table, tif_name, today_as_string)
+set_srs(filename)
